8c1462f9dfc625d37ee675102b4448b5c622856a
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-02-27 21:05:34 -0600

    Create Featured Jobs Section with cards

---
+++ Components/FeaturedJobs.js
@@ -0,0 +1,109 @@
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native'
+import React from 'react'
+import BGCards from '../assets/home/backgroundCard.png'
+import BookMark from '../assets/home/bookmark.png'
+import { useFonts, Inter_800ExtraBold, Inter_700Bold, Inter_600SemiBold, Inter_400Regular } from '@expo-google-fonts/inter'
+
+const FeaturedJobs = ({ data }) => {
+  return (
+    <View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <CardItem item={item} />}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  )
+}
+
+const CardItem = ({ item }) => {
+  const [fontsLoaded] = useFonts({
+    Inter_800ExtraBold,
+    Inter_700Bold,
+    Inter_600SemiBold,
+    Inter_400Regular,
+  })
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <TouchableOpacity activeOpacity={0.9} onPress={() => Alert.alert('See Job')}>
+      <View style={[style.card, { backgroundColor: item.bgColor }]}>
+        <Image source={BGCards} className='w-full h-full opacity-[0.1] absolute' />
+        <View className='flex-row items-center justify-evenly mt-[22px]'>
+          <View style={style.logo}>
+            <Image source={item.logo} className='w-[22px] h-[22px]' />
+          </View>
+          <View>
+            <Text style={style.position}>{item.position}</Text>
+            <Text style={style.company}>{item.company}</Text>
+          </View>
+          <View className='relative'>
+            <TouchableOpacity onPress={() => Alert.alert('Save Job')}>
+              <Image source={BookMark} style={style.bookmark} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View className='flex-row items-center justify-evenly mt-[24px]'>
+          {
+            item.categories.map(({ category, id }) => {
+              return (
+                <View style={style.categories} key={id}>
+                  <Text style={{ fontFamily: 'Inter_600SemiBold', color: 'white' }} className='text-[11px]'>{category}</Text>
+                </View>
+              )
+            })
+          }
+        </View>
+
+        <View className='flex-row items-center justify-around mt-[24px]'>
+          <Text style={{ fontFamily: 'Inter_600SemiBold', color: '#fff', fontSize: 13 }}>{item.payment}/{item.datePayment}</Text>
+          <Text style={{ fontFamily: 'Inter_600SemiBold', color: '#fff', fontSize: 13 }}>{item.location}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const style = StyleSheet.create({
+  card: {
+    width: 280,
+    height: 186,
+    marginLeft: 10,
+    borderRadius: 24,
+    position: 'relative'
+  },
+  logo: {
+    height: 46,
+    width: 46,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12
+  },
+  position: {
+    fontFamily: 'Inter_800ExtraBold',
+    fontSize: 16,
+    color: 'white'
+  },
+  company: {
+    fontFamily: 'Inter_600SemiBold',
+    color: 'white',
+    fontSize: 14
+  },
+  bookmark: {
+    width: 30,
+    height: 30,
+  },
+  categories: {
+    backgroundColor: 'rgba(255, 255, 255, 0.15)',
+    paddingVertical: 6,
+    paddingHorizontal: 16,
+    borderRadius: 65,
+  }
+})
+
+export default FeaturedJobs
---
+++ cards.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    company: 'Facebook',
+    logo: require('./assets/logo/Facebook.png'),
+    position: 'Junior Executive',
+    payment: 180000,
+    datePayment: 'year',
+    location: 'California, USA',
+    bgColor: '#5386E4',
+    categories: [
+      {
+        id: 1,
+        category: 'IT',
+      },
+      {
+        id: 2,
+        category: 'Full-Time',
+      },
+      {
+        id: 3,
+        category: 'Junior',
+      }
+    ]
+  },
+  {
+    id: 2,
+    company: 'Google',
+    logo: require('./assets/logo/google.png'),
+    position: 'Product Designer',
+    payment: 160000,
+    datePayment: 'year',
+    location: 'California, USA',
+    bgColor: '#356899',
+    categories: [
+      {
+        id: 1,
+        category: 'IT',
+      },
+      {
+        id: 2,
+        category: 'Full-Time',
+      },
+      {
+        id: 3,
+        category: 'Junior',
+      }
+    ]
+  },
+]
