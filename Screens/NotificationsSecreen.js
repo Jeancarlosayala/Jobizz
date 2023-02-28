94aeb7a6099213361ec303ef89724ced0a0b7b2d
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-02-27 22:17:38 -0600

    Init new Screens, messages, notifications and jobs saved

---
+++ Screens/NotificationsSecreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NotificationsSecreen = () => {
+  return (
+    <View>
+      <Text>NotificationsSecreen</Text>
+    </View>
+  )
+}
+
+export default NotificationsSecreen
