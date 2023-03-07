1771642b187c16558bfa9928d573b78c603ba310
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-06 21:10:42 -0600

    create sidebar options

---
+++ Api/sidebarOptions.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    option: 'Personal Info',
+    icon: require('../assets/icons/info.png')
+  },
+  {
+    id: 2,
+    option: 'Applications',
+    icon: require('../assets/icons/list.png')
+  },
+  {
+    id: 3,
+    option: 'Proposals',
+    icon: require('../assets/icons/listCheck.png')
+  },
+  {
+    id: 4,
+    option: 'Resumes',
+    icon: require('../assets/icons/resume.png')
+  },
+  {
+    id: 5,
+    option: 'Portfolio',
+    icon: require('../assets/icons/portfolio.png')
+  },
+  {
+    id: 6,
+    option: 'Cover Letters',
+    icon: require('../assets/icons/letter.png')
+  },
+  {
+    id: 7,
+    option: 'Settings',
+    icon: require('../assets/icons/setting.png')
+  },
+]
