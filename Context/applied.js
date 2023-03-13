04e85c9b48174e960cb3633a25e4c585ec0a257b
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-13 00:17:13 -0600

    create applied redux

---
+++ Context/applied.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  userAppliedJobs: null,
+}
+
+export const appliedJobs = createSlice ({
+  name: 'applied',
+  initialState,
+  reducers: {
+    setUserAppliedJobs:  (state, action) => {
+      state.userAppliedJobs = action.payload;
+    } 
+  }
+})
+
+export const {setUserAppliedJobs} = appliedJobs.actions;
+
+export const getAppliedJobs = state => state.applied.userAppliedJobs;
+
+export default appliedJobs.reducer;
