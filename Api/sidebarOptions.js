f41f4a6d4ad1beab45b9dab26b8951025a03aa0b
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-13 00:16:30 -0600

    add new option on sidebar

--- Api/sidebarOptions.js
+++ Api/sidebarOptions.js
@@ -2,36 +2,43 @@ export default [
   {
     id: 1,
     option: 'Personal Info',
-    icon: require('../assets/icons/info.png')
+    icon: require('../assets/icons/info.png'),
+    screen: ''
   },
   {
     id: 2,
     option: 'Applications',
-    icon: require('../assets/icons/list.png')
+    icon: require('../assets/icons/list.png'),
+    screen: 'Applications'
   },
   {
     id: 3,
     option: 'Proposals',
-    icon: require('../assets/icons/listCheck.png')
+    icon: require('../assets/icons/listCheck.png'),
+    screen: ''
   },
   {
     id: 4,
     option: 'Resumes',
-    icon: require('../assets/icons/resume.png')
+    icon: require('../assets/icons/resume.png'),
+    screen: ''
   },
   {
     id: 5,
     option: 'Portfolio',
-    icon: require('../assets/icons/portfolio.png')
+    icon: require('../assets/icons/portfolio.png'),
+    screen: ''
   },
   {
     id: 6,
     option: 'Cover Letters',
-    icon: require('../assets/icons/letter.png')
+    icon: require('../assets/icons/letter.png'),
+    screen: ''
   },
   {
     id: 7,
     option: 'Settings',
-    icon: require('../assets/icons/setting.png')
+    icon: require('../assets/icons/setting.png'),
+    screen: ''
   },
 ]
