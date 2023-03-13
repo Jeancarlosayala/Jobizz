723a2dfadb8961d279daef8977c07445fc614130
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-13 00:17:21 -0600

    create applied redux

--- store.js
+++ store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appliedReducer from "./Context/applied";
 import userReducer from './Context/user'
 
 export const store = configureStore({
   reducer:{
     user: userReducer,
+    applied: appliedReducer
   }
 })
