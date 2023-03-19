8089e89f3136e12977a3de43e0b6be783a3bb629
Jeancarlosayala <jeancarlosayala57@gmail.com>
2023-03-19 00:36:38 -0600

    create new route ApplyJob

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
-import Onboarding from '@Components/Onboarding';
-import { setUser } from '@Context/user';
-import ApplicationsScreen from '@Screens/ApplicationsScreen/ApplicationsScreen';
-import JobDetailScreen from '@Screens/JobDetail/JobDetailScreen';
-import Login from '@Screens/Login';
-import Main from '@Screens/Main';
-import Profile from '@Screens/Profile';
-import Register from '@Screens/Register';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
+import { Provider, useDispatch } from "react-redux";
+import Onboarding from "@Components/Onboarding";
+import { setUser } from "@Context/user";
 
-import { store } from './store';
+import ApplicationsScreen from "@Screens/ApplicationsScreen/ApplicationsScreen";
+import JobDetailScreen from "@Screens/JobDetail/JobDetailScreen";
+import Login from "@Screens/Login";
+import Main from "@Screens/Main";
+import Profile from "@Screens/Profile";
+import Register from "@Screens/Register";
+import ApplyJob from "@Screens/ApplyJob/ApplyJob";
+
+import { store } from "./store";
+import { setUserAppliedJobs } from "./Context/applied";
 
 const Stack = createNativeStackNavigator();
 
@@ -26,27 +29,63 @@ export default function App() {
 }
 
 const Views = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getSession = async () => {
-      await fetch('http://192.168.1.4:4000/api/jobizz/user/login/')
-        .then(res => res.json())
-        .then(data => dispatch(setUser(data)))
-        .catch(err => console.log(err))
-    }
+      await fetch("http://192.168.1.4:4000/api/jobizz/user/login/")
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setUser(data));
+
+          fetch(`http://192.168.1.4:4000/api/jobizz/applied/isapplied/${data.data.id}`)
+            .then((res) => res.json())
+            .then((data) => dispatch(setUserAppliedJobs(data)))
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+    };
     getSession();
-  }, [])
+  }, []);
 
   return (
     <Stack.Navigator>
       {/* <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}} /> */}
-      <Stack.Screen name="Main" component={Main} options={{ headerShown: false, presentation: 'fullScreenModal' }} />
-      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-      <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
-      <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false, presentation: 'fullScreenModal' }} />
-      <Stack.Screen name="JobDetail" component={JobDetailScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Applications" component={ApplicationsScreen} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{ headerShown: false, presentation: "fullScreenModal" }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{ headerShown: false, presentation: "fullScreenModal" }}
+      />
+      <Stack.Screen
+        name="JobDetail"
+        component={JobDetailScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Applications"
+        component={ApplicationsScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ApplyJob"
+        component={ApplyJob}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
-  )
-}
+  );
+};
---
+++ Screens/ApplyJob/ApplyJob.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ApplyJob = () => {
+  return (
+    <View>
+      <Text>ApplyJob</Text>
+    </View>
+  )
+}
+
+export default ApplyJob
