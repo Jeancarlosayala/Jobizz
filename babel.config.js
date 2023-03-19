80f5fbfb6fe4d1720153b5e1b98cc2306aaf97fc
Jeancarlosayala <jeancarlosayala57@gmail.com>
2023-03-18 23:27:01 -0600

    install babel plugin module and config routes alias

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@assets": "./assets",
+            "@Components": './Components',
+            "@Screens": './Screens',
+            "@Context": './Context',
+          },
+        },
+      ],
+    ],
   };
 };
