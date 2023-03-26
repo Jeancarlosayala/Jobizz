020e2a513a9136427dca31d7876c32b42e355d7e
Jeancarlosayala <jeancarlosayala57@gmail.com>
2023-03-26 00:14:32 -0600

    applyJob design

--- Screens/ApplyJob/ApplyJob.js
+++ Screens/ApplyJob/ApplyJob.js
@@ -1,12 +1,112 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+} from "react-native";
+import { ApStyles } from "./ApplyJobStyle";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { getUser } from "@Context/user";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import Back from "@assets/icons/back_black.png";
+import PopularJobs from "@Components/PrimaryCard/PopularJobs";
+import Checkbox from "expo-checkbox";
+import Button from "@Components/Button/Button";
 
 const ApplyJob = () => {
-  return (
-    <View>
-      <Text>ApplyJob</Text>
-    </View>
-  )
-}
+  const [ischecked, setIschecked] = useState(false);
+  const { params } = useRoute();
+  const navigation = useNavigation();
+  const data = [params.item];
+  const user = useSelector(getUser);
+  const { name, image } = user.data;
 
-export default ApplyJob
+  const alert = () =>{
+    Alert.alert('hola')
+  }
+
+  return (
+    <View className="px-[24px] bg-[#FAFAFD] h-full">
+      <SafeAreaView className="bg-[#fff] mb-[24px]">
+        <View className="flex-row items-center w-full mb-[24px]">
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image style={ApStyles.icon} source={Back} />
+          </TouchableOpacity>
+          <Text style={ApStyles.headerText} className="ml-[123px]">
+            Apply
+          </Text>
+        </View>
+        <View>
+          <PopularJobs height={44} data={data} />
+        </View>
+      </SafeAreaView>
+
+      <KeyboardAwareScrollView>
+        <View className="mb-[32px]">
+          <Text style={ApStyles.headerText}>Your profile</Text>
+
+          <View className="items-center">
+            <View
+              className="bg-[#fff] h-[155px] w-[155px] 
+          items-center justify-center rounded-[16px] space-y-[4px]"
+            >
+              <Image style={ApStyles.image} source={{ uri: image }} />
+              <Text style={ApStyles.headerText}>{name}</Text>
+              <Text style={ApStyles.subText}>Front End Developer</Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="mb-[40px]">
+          <Text style={ApStyles.headerText}>Resume Selected</Text>
+
+          <View
+            className="flex-row items-center justify-center 
+        mt-[16px] h-[58px] w-[155px] bg-[#fff] rounded-[16px]"
+          >
+            <Checkbox
+              style={ApStyles.checkbox}
+              value={ischecked}
+              onValueChange={setIschecked}
+              color={ischecked ? "#5386E4" : "#CACBCE"}
+            />
+            <View className="ml-[20px] space-y-[8px]">
+              <View style={ApStyles.role}>
+                <Text style={ApStyles.roleText}>Front End</Text>
+              </View>
+              <Text style={[ApStyles.headerText, { fontSize: 14 }]}>
+                {name}
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View className='mb-[32px]'>
+          <Text style={ApStyles.headerText} className="mb-[16px]">
+            Cover Later
+            <Text style={ApStyles.optionalText}>(Optional)</Text>
+          </Text>
+
+          <TextInput
+            multiline={true}
+            numberOfLines={4}
+            placeholder="Write a cover letter......"
+            style={ApStyles.textArea}
+          />
+        </View>
+
+        <Button styles={'primary'} onPress={alert}>
+          <Text style={ApStyles.whiteText}>Apply</Text>
+        </Button>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+};
+
+export default ApplyJob;
--- Screens/ApplyJob/ApplyJobStyle.js
+++ Screens/ApplyJob/ApplyJobStyle.js
@@ -1,5 +1,59 @@
 import { StyleSheet } from "react-native";
 
 export const ApStyles = StyleSheet.create({
-    
-})
+  icon: {
+    height: 24,
+    width: 24,
+    resizeMode: "contain",
+  },
+  headerText: {
+    fontSize: 16,
+    fontFamily: "Inter_600SemiBold",
+  },
+  subText: {
+    fontFamily: "Inter_300Light",
+    color: "#95969D",
+  },
+  image: {
+    height: 72,
+    width: 72,
+    borderRadius: 100,
+  },
+  checkbox: {
+    borderRadius: 100,
+    height: 20,
+    width: 20,
+    borderColor: "#CACBCE",
+    resizeMode: "contain",
+  },
+  role: {
+    backgroundColor: "#356899",
+    height: 25,
+    paddingHorizontal: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+  },
+  roleText: {
+    color: "#fff",
+    fontSize: 12,
+    fontFamily: "Inter_500Medium",
+  },
+  optionalText: {
+    color: "#95969D",
+    fontFamily: "Inter_300Light",
+    fontSize: 16,
+  },
+  textArea: {
+    height: 89,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  whiteText: {
+    color: "#fff",
+    fontFamily: "Inter_500Medium",
+    fontSize: 16,
+  },
+});
